83daeaba7d8e8472d990482a01ad608f9892402f
imkomal <komalcool.sinha@gmail.com>
2017-06-02 00:56:22 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,146 @@
+package com.komal.gallery;
+
+import java.util.ArrayList;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.MediaStore.MediaColumns;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends Activity {
+
+    public static ArrayList<Model_images> al_images = new ArrayList<>();
+    boolean boolean_folder;
+    Adapter_PhotosFolder obj_adapter;
+    GridView gv_folder;
+    private static final int REQUEST_PERMISSIONS = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gv_folder = (GridView)findViewById(R.id.gv_folder);
+
+        gv_folder.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), PhotosActivity.class);
+                intent.putExtra("value",i);
+                startActivity(intent);
+            }
+        });
+
+
+        if ((ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) && (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE))) {
+
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSIONS);
+            }
+        }else {
+            Log.e("Else","Else");
+            fn_imagespath();
+        }
+
+
+
+    }
+
+    public ArrayList<Model_images> fn_imagespath() {
+        al_images.clear();
+
+        int int_position = 0;
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+
+        String absolutePathOfImage = null;
+        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+
+        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+        cursor = getApplicationContext().getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while (cursor.moveToNext()) {
+            absolutePathOfImage = cursor.getString(column_index_data);
+            Log.e("Column", absolutePathOfImage);
+            Log.e("Folder", cursor.getString(column_index_folder_name));
+
+            for (int i = 0; i < al_images.size(); i++) {
+                if (al_images.get(i).getStr_folder().equals(cursor.getString(column_index_folder_name))) {
+                    boolean_folder = true;
+                    int_position = i;
+                    break;
+                } else {
+                    boolean_folder = false;
+                }
+            }
+
+
+            if (boolean_folder) {
+
+                ArrayList<String> al_path = new ArrayList<>();
+                al_path.addAll(al_images.get(int_position).getAl_imagepath());
+                al_path.add(absolutePathOfImage);
+                al_images.get(int_position).setAl_imagepath(al_path);
+
+            } else {
+                ArrayList<String> al_path = new ArrayList<>();
+                al_path.add(absolutePathOfImage);
+                Model_images obj_model = new Model_images();
+                obj_model.setStr_folder(cursor.getString(column_index_folder_name));
+                obj_model.setAl_imagepath(al_path);
+
+                al_images.add(obj_model);
+
+
+            }
+
+
+        }
+
+
+        for (int i = 0; i < al_images.size(); i++) {
+            Log.e("FOLDER", al_images.get(i).getStr_folder());
+            for (int j = 0; j < al_images.get(i).getAl_imagepath().size(); j++) {
+                Log.e("FILE", al_images.get(i).getAl_imagepath().get(j));
+            }
+        }
+        obj_adapter = new Adapter_PhotosFolder(getApplicationContext(),al_images);
+        gv_folder.setAdapter(obj_adapter);
+        return al_images;
+    }
+
+
+
+}
+
+
